0b85cda35649437c7f1474a244fbb03f6ac0e9cb
Kelvin Keter <33624862+kelvinketer@users.noreply.github.com>
2025-12-26 01:58:35 +0300

    Register Analytics in Admin Panel

--- cars/admin.py
+++ cars/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Car, CarImage
+from .models import Car, CarImage, CarAnalytics # <--- Added CarAnalytics import
 
 class CarImageInline(admin.TabularInline):
     model = CarImage
@@ -11,3 +11,11 @@ class CarAdmin(admin.ModelAdmin):
     list_display = ('make', 'model', 'year', 'price', 'status', 'dealer')
     list_filter = ('status', 'make', 'transmission')
     search_fields = ('make', 'model', 'dealer__username')
+
+# --- NEW: REGISTER ANALYTICS ---
+@admin.register(CarAnalytics)
+class CarAnalyticsAdmin(admin.ModelAdmin):
+    list_display = ('action', 'car', 'timestamp', 'ip_address') # Columns to show
+    list_filter = ('action', 'timestamp') # Sidebar filters
+    search_fields = ('car__make', 'car__model') # Search bar logic
+    readonly_fields = ('timestamp', 'ip_address', 'action', 'car') # Prevent editing history
