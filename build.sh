58965518a647370a28ef5b5a118e02b2f637a76b
Kelvin Keter <33624862+kelvinketer@users.noreply.github.com>
2025-12-25 15:49:33 +0300

    Remove nuke script to fix SQLite error

--- build.sh
+++ build.sh
@@ -5,16 +5,11 @@ set -o errexit
 # 1. Install dependencies
 pip install -r requirements.txt
 
-# 2. Convert static files (CSS/Images)
+# 2. Convert static files
 python manage.py collectstatic --no-input
 
-# --- THE DANGER ZONE (Run Once) ---
-# This wipes the cloud database completely so we can fix the migration conflict.
-# MAKE SURE 'nuke_db.py' EXISTS BEFORE PUSHING!
-python nuke_db.py
-# ----------------------------------
-
-# 3. Apply database migrations (Re-creates clean tables)
+# 3. Apply database migrations
+# Since you are on SQLite, this creates a fresh DB every time automatically!
 python manage.py migrate
 
 # 4. Auto-Create Admin User
