ad88a92a04f9071ff9c50a79f5ed5e08ba7b511c
Kelvin Keter <33624862+kelvinketer@users.noreply.github.com>
2026-02-04 15:55:00 +0300

    Fix: Import user_passes_test decorator

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.admin.views.decorators import staff_member_required
 from django.views.decorators.http import require_POST 
 from django.contrib import messages
